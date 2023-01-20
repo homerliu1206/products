f866320a5c669e66fefbb56e84df71d2aa1c1c5a
Homer Liu <kingliu06@gmail.com>
2023-01-21 01:25:15 +0800

    write to file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#讀取檔案
 products = []
+with open ('products.csv', 'r') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue  #跳至下一個迴圈<只能寫在迴圈中>
+		name, price = line.strip().split(',')
+		products.append([name,price])
+print (products)
+
+#讓使用者輸入
 while True:
 	name = input ('請輸入商品名稱:')
 	if name == 'q' or name == 'Q':  #quit
@@ -9,23 +19,14 @@ while True:
 	p.append(name)
 	p.append(price)
 	products.append(p)
-	
 print (products)
 
-
+#印出所有購買紀錄
 for p in products:
 	print (p[0], '的價格是', p[1])
 
-with open('products.csv', 'w') as f:  #'w'表「寫入模式」
-#若已有檔案，會以新檔案覆蓋
-#若無檔案則會新增一個新檔案
-
+#寫入檔案
+with open('products.csv', 'w', encoding='utf-8') as f: 
 	f.write('商品'+ ',' + '價格' + '\n')
-	
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
-		# 此行才是真正的寫入動作
-		# 'abc' + '123' = 'abc123'
-		# 'abc' * 3 = 'abcabcabc'
-
-
