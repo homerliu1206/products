1c736a30f0dc4906528778b79ee0a947a310b8f9
Homer Liu <kingliu06@gmail.com>
2023-01-21 01:02:01 +0800

    add header

--- products.py
+++ products.py
@@ -15,5 +15,17 @@ print (products)
 
 for p in products:
 	print (p[0], '的價格是', p[1])
+
+with open('products.csv', 'w') as f:  #'w'表「寫入模式」
+#若已有檔案，會以新檔案覆蓋
+#若無檔案則會新增一個新檔案
+
+	f.write('商品'+ ',' + '價格' + '\n')
 	
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+		# 此行才是真正的寫入動作
+		# 'abc' + '123' = 'abc123'
+		# 'abc' * 3 = 'abcabcabc'
+
 
